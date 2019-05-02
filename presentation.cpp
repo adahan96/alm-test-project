a4d06dd6e4a2e15ae24daf1c13bd0e2414f78a35
Adahan Yalçınkaya <36035951+adahan96@users.noreply.github.com>
2019-05-02 17:05:53 +0300

    Median program

---
+++ presentation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(int argc, char** argv){
+    int median;
+    if(argc % 2 == 0)
+        median = atoi(argv[argc/2]);
+    else
+        median = (atoi(argv[(argc/2)]) +  atoi(argv[(argc/2) + 1])) / 2;
+    
+	cout << median;
+	
+    return 0; 
+}
