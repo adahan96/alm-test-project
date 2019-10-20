fa366e037601a0c1e9607531efb529e06ce486ac
Yasin Balcancı <36000428+ybalcanci@users.noreply.github.com>
2019-10-20 22:37:31 +0300

    At least 1 argument is mandatory to calculate a median.

--- presentation.cpp
+++ presentation.cpp
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 using namespace std;
 int main(int argc, char** argv){
+    if(argc < 2){
+	cout << "You provided unsufficient number of parameters." << endl;
+	return -1;
+    }
     int median;
     if(argc % 2 == 0)
         median = atoi(argv[argc/2]);
     else
         median = (atoi(argv[(argc/2)]) +  atoi(argv[(argc/2) + 1])) / 2;
-	cout << median;
+	cout << median << endl;
     return 0; 
 }
